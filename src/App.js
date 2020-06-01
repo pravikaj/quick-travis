c81e2947ad88618cf23e26d20ccf49ebe39927bb
pravikaj <pravika.joshi@gmail.com>
2020-06-01 16:28:30 -0500

    fixed App.test.js file

--- src/App.js
+++ src/App.js
@@ -1,56 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Goal from './Card/Goal'
-import AddGoal from './AddGoal';
-import GoalGrid from './GoalGrid';
-import firebase from './shared/firebase'
-import ToggleButtons from './Toggle'
-import Badge from '@material-ui/core/Badge'; 
-import MailIcon from '@material-ui/icons/Mail'; 
-import HomeIcon from '@material-ui/icons/Home'; 
-import ArchiveIcon from '@material-ui/icons/Archive'; 
-import IconButton from '@material-ui/core/IconButton'; 
-import Tooltip from '@material-ui/core/Tooltip';
+import React, { useEffect, useState } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+// import Goal from "./Card/Goal";
+import AddGoal from "./AddGoal";
+import GoalGrid from "./GoalGrid";
+import firebase from "./shared/firebase";
+// import ToggleButtons from './Toggle'
+import Badge from "@material-ui/core/Badge";
 
+import MailIcon from "@material-ui/icons/Mail";
+
+import HomeIcon from "@material-ui/icons/Home";
+
+import ArchiveIcon from "@material-ui/icons/Archive";
+
+import IconButton from "@material-ui/core/IconButton";
+
+import Tooltip from "@material-ui/core/Tooltip";
 
 const db = firebase.database().ref();
 
 const uiConfig = {
-  signInFlow: 'popup',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   callbacks: {
-    signInSuccessWithAuthResult: () => false
-  }
+    signInSuccessWithAuthResult: () => false,
+  },
 };
 
 const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-  icons:{
-    margin:"5px",
-    color: "white"
-  }
+  icons: {
+    margin: "5px",
+    color: "white",
+  },
 }));
 
-const Welcome = ({ user, invites, setGridView}) => {
+const Welcome = ({ user, invites, setGridView }) => {
   const classes = useStyles();
   let count = 0;
-  Object.values(invites).map(goal=>goal.confirmed?null:count++);
+  Object.values(invites).map((goal) => (goal.confirmed ? null : count++));
 
   return (
     <React.Fragment>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h5" className={classes.title} onClick={() => {window.location.reload(false)}}>
+          <Typography
+            variant="h5"
+            className={classes.title}
+            onClick={() => {
+              window.location.reload(false);
+            }}
+          >
             Work2Gather
           </Typography>
           <Tooltip title="Home">
@@ -60,28 +68,39 @@ const Welcome = ({ user, invites, setGridView}) => {
           </Tooltip>
           <Tooltip title="Invites">
             <IconButton onClick={() => setGridView("INVITES")}>
-              <Badge 
+              <Badge
                 anchorOrigin={{
-                   vertical: 'top',
-                  horizontal: 'left',
-                }} 
-                badgeContent={count} 
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                badgeContent={count}
                 color="secondary"
-                className={classes.icons}>
+                className={classes.icons}
+              >
                 <MailIcon />
               </Badge>
             </IconButton>
           </Tooltip>
           <Tooltip title="Archive">
-            <IconButton  onClick={() => setGridView("ARCHIVE")}>
-              <ArchiveIcon className={classes.icons}/>
+            <IconButton onClick={() => setGridView("ARCHIVE")}>
+              <ArchiveIcon className={classes.icons} />
             </IconButton>
           </Tooltip>
-          
-          <Typography variant="h6" style={{ marginLeft:"10px", float: "center", marginRight: 30 }}>
-            Welcome, {user.displayName ? user.displayName.split(' ')[0] : ""}
+
+          <Typography
+            variant="h6"
+            style={{ marginLeft: "10px", float: "center", marginRight: 30 }}
+          >
+            Welcome, {user.displayName ? user.displayName.split(" ")[0] : ""}
           </Typography>
-          <Button style={{ fontSize: 21 }} color="inherit" onClick={() => { setGridView("ACTIVE"); firebase.auth().signOut() }}>
+          <Button
+            style={{ fontSize: 21 }}
+            color="inherit"
+            onClick={() => {
+              setGridView("ACTIVE");
+              firebase.auth().signOut();
+            }}
+          >
             Log out
           </Button>
         </Toolbar>
@@ -91,15 +110,16 @@ const Welcome = ({ user, invites, setGridView}) => {
 };
 
 const SignIn = () => (
-  <StyledFirebaseAuth
-    uiConfig={uiConfig}
-    firebaseAuth={firebase.auth()}
-  />
+  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
 );
 
 const Banner = ({ user, invites, title, setGridView }) => (
   <React.Fragment>
-    {user ? <Welcome user={user} invites={invites} setGridView={setGridView}/> : <SignIn />}
+    {user ? (
+      <Welcome user={user} invites={invites} setGridView={setGridView} />
+    ) : (
+      <SignIn />
+    )}
   </React.Fragment>
 );
 
@@ -118,28 +138,36 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    const handleData = snap => {
+    const handleData = (snap) => {
       if (user) {
         if (snap.val()) {
           let re = /\./gi;
-          let email = user.email.replace(re, ',')
-          db.child('emailTouid/' + email).set(user.uid);
+          let email = user.email.replace(re, ",");
+          db.child("emailTouid/" + email).set(user.uid);
           setEmailTouid(snap.val().emailTouid);
           if (!snap.val().users[user.uid]) {
-            db.child("users").child(user.uid).child("name").set(user.displayName);
+            db.child("users")
+              .child(user.uid)
+              .child("name")
+              .set(user.displayName);
           }
           if (snap.val().users[user.uid] && snap.val().users[user.uid].goals) {
             let goals_arr = snap.val().users[user.uid].goals;
-            setGoals(Object.values(goals_arr).map(goal => snap.val().goals[goal]));
-          }
-          else {
+            setGoals(
+              Object.values(goals_arr).map((goal) => snap.val().goals[goal])
+            );
+          } else {
             setGoals({});
           }
-          if (snap.val().users[user.uid] && snap.val().users[user.uid].invites) {
+          if (
+            snap.val().users[user.uid] &&
+            snap.val().users[user.uid].invites
+          ) {
             let invites_arr = snap.val().users[user.uid].invites;
-            setInvite(Object.values(invites_arr).map(goal => snap.val().goals[goal]));
-          }
-          else {
+            setInvite(
+              Object.values(invites_arr).map((goal) => snap.val().goals[goal])
+            );
+          } else {
             setInvite({});
           }
         }
@@ -148,21 +176,41 @@ const App = () => {
         setEmailTouid({});
       }
     };
-    db.on('value', handleData, error => alert(error));
-    return () => { db.off('value', handleData); };
+    db.on("value", handleData, (error) => alert(error));
+    return () => {
+      db.off("value", handleData);
+    };
   }, [user]);
 
   return (
     <div>
-      <Banner user={user} invites={invites} title="Work2Gather" setGridView={setGridView}>
-      </Banner>
+      <Banner
+        user={user}
+        invites={invites}
+        title="Work2Gather"
+        setGridView={setGridView}
+      ></Banner>
       <br></br>
-      {user ? <AddGoal open={open} user={user} setOpen={setOpen} emailTouid={emailTouid} /> : null}
-      {user ? <Container maxWidth="xl">
-        <GoalGrid goals={goals} invites={invites} user={user} gridView={gridView}/>
-      </Container> : null}
+      {user ? (
+        <AddGoal
+          open={open}
+          user={user}
+          setOpen={setOpen}
+          emailTouid={emailTouid}
+        />
+      ) : null}
+      {user ? (
+        <Container maxWidth="xl">
+          <GoalGrid
+            goals={goals}
+            invites={invites}
+            user={user}
+            gridView={gridView}
+          />
+        </Container>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
